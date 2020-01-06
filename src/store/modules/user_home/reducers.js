c2277e8f5c79fa9237b86accb251cbbffe2a2691
Silas Onyango <silas.onyango93@gmail.com>
2020-01-06 20:08:33 +0300

    transition to loan details

--- src/store/modules/user_home/reducers.js
+++ src/store/modules/user_home/reducers.js
@@ -1,9 +1,16 @@
-import {CLIENT_DETAILS_SUBMITTED_SUCCESSFULLY} from "./actionTypes";
+import {CLIENT_DETAILS_SUBMITTED_SUCCESSFULLY, CLIENT_EMPLOYMENT_DETAILS_UPDATE_SUCCESSFUL} from "./actionTypes";
 
 export const ACTION_HANDLERS = {
     [CLIENT_DETAILS_SUBMITTED_SUCCESSFULLY]: state =>
         Object.assign({}, state, {
             displayPersonalDetails: false,
-            displayEmploymentDetails: true
+            displayEmploymentDetails: true,
+            displayLoanDetails: false
+        }),
+    [CLIENT_EMPLOYMENT_DETAILS_UPDATE_SUCCESSFUL]: state =>
+        Object.assign({}, state, {
+            displayPersonalDetails: false,
+            displayEmploymentDetails: false,
+            displayLoanDetails: true
         })
 };
--- src/views/user_home/data_entry/DataEntryDialog.jsx
+++ src/views/user_home/data_entry/DataEntryDialog.jsx
@@ -6,9 +6,10 @@ import {connect} from "react-redux";
 import './DataEntryDialog.scss';
 import PersonalDetails from "./personal_details/PersonalDetails";
 import EmploymentDetails from "./employment_details/EmploymentDetails";
+import LoanDetails from "./loan_details/LoanDetails";
 
 class DataEntryDialog extends Component {
-    state = {visible: true, displayPersonalDetails: true, displayEmploymentDetails: false};
+    state = {visible: true, displayPersonalDetails: true, displayEmploymentDetails: false, displayLoanDetails: false};
 
     componentDidUpdate(prevProps) {
         if(this.props.displayPersonalDetails !== prevProps.displayPersonalDetails) {
@@ -17,6 +18,10 @@ class DataEntryDialog extends Component {
 
         if(this.props.displayEmploymentDetails !== prevProps.displayEmploymentDetails) {
             this.setState({displayEmploymentDetails: this.props.displayEmploymentDetails});
+        }
+
+        if(this.props.displayLoanDetails !== prevProps.displayLoanDetails) {
+            this.setState({displayLoanDetails: this.props.displayLoanDetails});
         }
     }
 
@@ -42,6 +47,9 @@ class DataEntryDialog extends Component {
                     <div className={this.state.displayEmploymentDetails ? 'show' : 'hide'}>
                         <EmploymentDetails />
                     </div>
+                    <div className={this.state.displayLoanDetails ? 'show' : 'hide'}>
+                        <LoanDetails />
+                    </div>
 
                 </Modal>
             </div>
@@ -50,12 +58,15 @@ class DataEntryDialog extends Component {
 }
 
 DataEntryDialog.propTypes = {
-    displayPersonalDetails: PropTypes.bool.isRequired
+    displayPersonalDetails: PropTypes.bool.isRequired,
+    displayEmploymentDetails: PropTypes.bool.isRequired,
+    displayLoanDetails: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = state => ({
     displayPersonalDetails: state.user_home.displayPersonalDetails,
-    displayEmploymentDetails: state.user_home.displayEmploymentDetails
+    displayEmploymentDetails: state.user_home.displayEmploymentDetails,
+    displayLoanDetails: state.user_home.displayLoanDetails
 });
 
 export default connect(
