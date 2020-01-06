5caab2852f2a5149f58655cf01c70b809ff47dae
Silas Onyango <silas.onyango93@gmail.com>
2020-01-06 23:41:50 +0300

    loan details ui

--- src/views/user_home/data_entry/DataEntryDialog.jsx
+++ src/views/user_home/data_entry/DataEntryDialog.jsx
@@ -1,75 +1,103 @@
-import React, {Component} from 'react';
-import Modal from 'react-awesome-modal';
+import React, { Component } from "react";
+import Modal from "react-awesome-modal";
 import PropTypes from "prop-types";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 
-import './DataEntryDialog.scss';
+import "./DataEntryDialog.scss";
 import PersonalDetails from "./personal_details/PersonalDetails";
 import EmploymentDetails from "./employment_details/EmploymentDetails";
 import LoanDetails from "./loan_details/LoanDetails";
 
 class DataEntryDialog extends Component {
-    state = {visible: true, displayPersonalDetails: true, displayEmploymentDetails: false, displayLoanDetails: false};
+  state = {
+    visible: true,
+    displayPersonalDetails: false,
+    displayEmploymentDetails: false,
+    displayLoanDetails: false
+  };
 
-    componentDidUpdate(prevProps) {
-        if(this.props.displayPersonalDetails !== prevProps.displayPersonalDetails) {
-            this.setState({displayPersonalDetails: this.props.displayPersonalDetails});
-        }
+  componentDidMount() {
+    this.setState({
+      displayPersonalDetails: this.props.displayPersonalDetails,
+      displayEmploymentDetails: this.props.displayEmploymentDetails,
+      displayLoanDetails: this.props.displayLoanDetails
+    });
+  }
 
-        if(this.props.displayEmploymentDetails !== prevProps.displayEmploymentDetails) {
-            this.setState({displayEmploymentDetails: this.props.displayEmploymentDetails});
-        }
-
-        if(this.props.displayLoanDetails !== prevProps.displayLoanDetails) {
-            this.setState({displayLoanDetails: this.props.displayLoanDetails});
-        }
+  componentDidUpdate(prevProps) {
+    if (
+      this.props.displayPersonalDetails !== prevProps.displayPersonalDetails
+    ) {
+      this.setState({
+        displayPersonalDetails: this.props.displayPersonalDetails
+      });
     }
 
-    closeModal = () => {
-        this.setState({visible: false});
-    };
-
-    openModal = () => {
-        this.setState({visible: true});
-    };
-
-    render() {
-        return (
-            <div>
-                <Modal visible={this.state.visible} width="600" height="630" effect="fadeInUp" onClickAway={() => this.closeModal()}>
-                    <div className="success-modal-header">
-                        <p className="modal-title">Client Registration</p>
-                    </div>
-
-                    <div className={this.state.displayPersonalDetails ? 'show' : 'hide'}>
-                        <PersonalDetails />
-                    </div>
-                    <div className={this.state.displayEmploymentDetails ? 'show' : 'hide'}>
-                        <EmploymentDetails />
-                    </div>
-                    <div className={this.state.displayLoanDetails ? 'show' : 'hide'}>
-                        <LoanDetails />
-                    </div>
-
-                </Modal>
-            </div>
-        );
+    if (
+      this.props.displayEmploymentDetails !== prevProps.displayEmploymentDetails
+    ) {
+      this.setState({
+        displayEmploymentDetails: this.props.displayEmploymentDetails
+      });
     }
+
+    if (this.props.displayLoanDetails !== prevProps.displayLoanDetails) {
+      this.setState({ displayLoanDetails: this.props.displayLoanDetails });
+    }
+  }
+
+  closeModal = () => {
+    this.setState({ visible: false });
+  };
+
+  openModal = () => {
+    this.setState({ visible: true });
+  };
+
+  render() {
+    return (
+      <div>
+        <Modal
+          visible={this.state.visible}
+          width="600"
+          height="630"
+          effect="fadeInUp"
+          onClickAway={() => this.closeModal()}
+        >
+          <div className="success-modal-header">
+            <p className="modal-title">Client Registration</p>
+          </div>
+
+          <div className={this.state.displayPersonalDetails ? "show" : "hide"}>
+            <PersonalDetails />
+          </div>
+          <div
+            className={this.state.displayEmploymentDetails ? "show" : "hide"}
+          >
+            <EmploymentDetails />
+          </div>
+          <div className={this.state.displayLoanDetails ? "show" : "hide"}>
+            <LoanDetails />
+          </div>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 DataEntryDialog.propTypes = {
-    displayPersonalDetails: PropTypes.bool.isRequired,
-    displayEmploymentDetails: PropTypes.bool.isRequired,
-    displayLoanDetails: PropTypes.bool.isRequired
+  displayPersonalDetails: PropTypes.bool.isRequired,
+  displayEmploymentDetails: PropTypes.bool.isRequired,
+  displayLoanDetails: PropTypes.bool.isRequired
 };
 
 const mapStateToProps = state => ({
-    displayPersonalDetails: state.user_home.displayPersonalDetails,
-    displayEmploymentDetails: state.user_home.displayEmploymentDetails,
-    displayLoanDetails: state.user_home.displayLoanDetails
+  displayPersonalDetails: state.user_home.displayPersonalDetails,
+  displayEmploymentDetails: state.user_home.displayEmploymentDetails,
+  displayLoanDetails: state.user_home.displayLoanDetails
 });
 
 export default connect(
-    mapStateToProps,
-    null
+  mapStateToProps,
+  null
 )(DataEntryDialog);
--- src/views/user_home/data_entry/loan_details/LoanDetails.jsx
+++ src/views/user_home/data_entry/loan_details/LoanDetails.jsx
@@ -1,13 +1,145 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { Columns } from "react-bulma-components/dist";
 
 class LoanDetails extends Component {
-    render() {
-        return (
-            <div>
+  state = {
+    loanAmount: "",
+    interestRate: "",
+    remainingLoanAmount: "",
+    loanAmountHasError: false,
+    loanAmountErrorMessage: "",
+    interestRateHasError: false,
+    interestRateErrorMessage: "",
+    remainingLoanAmountHasError: false,
+    remainingLoanAmountErrorMessage: ""
+  };
 
-            </div>
-        );
-    }
+  handleChange = event => {
+    let newState = this.state;
+    newState[event.target.name] = event.target.value;
+    this.setState({
+      ...newState
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="login-panel panel panel-default dialog__main-body">
+          <div className="panel-heading">
+            <h3 className="panel-title">Loan Details</h3>
+          </div>
+          <div className="panel-body">
+            <form
+              action=""
+              method="POST"
+              onSubmit={this.handleSubmit}
+              encType="multipart/form-data"
+            >
+              <fieldset>
+                <Columns>
+                  <Columns.Column size="one-half">
+                    <div className="form-group">
+                      <input
+                        name="loanAmount"
+                        className={
+                          this.state.loanAmountHasError
+                            ? "form-control personal__text-area-error"
+                            : "form-control"
+                        }
+                        placeholder="Loan Amount"
+                        value={this.state.loanAmount}
+                        type="number"
+                        onChange={this.handleChange}
+                        autoFocus
+                        required={true}
+                      />
+                      <p
+                        className={
+                          this.state.loanAmountHasError
+                            ? "personal__submision-error"
+                            : "personal__hide"
+                        }
+                      >
+                        {this.state.loanAmountErrorMessage}
+                      </p>
+                    </div>
+                  </Columns.Column>
+
+                  <Columns.Column size="one-half">
+                    <div className="form-group">
+                      <input
+                        name="interestRate"
+                        className={
+                          this.state.interestRateHasError
+                            ? "form-control personal__text-area-error"
+                            : "form-control"
+                        }
+                        placeholder="Interest Rate"
+                        value={this.state.interestRate}
+                        type="number"
+                        onChange={this.handleChange}
+                        autoFocus
+                        required={true}
+                      />
+                      <p
+                        className={
+                          this.state.interestRateHasError
+                            ? "personal__submision-error"
+                            : "personal__hide"
+                        }
+                      >
+                        {this.state.interestRateErrorMessage}
+                      </p>
+                    </div>
+                  </Columns.Column>
+                </Columns>
+
+                <Columns>
+                  <Columns.Column size="one-half">
+                    <div className="form-group">
+                      <input
+                        name="remainingLoanAmount"
+                        className={
+                          this.state.remainingLoanAmountHasError
+                            ? "form-control personal__text-area-error"
+                            : "form-control"
+                        }
+                        placeholder="Remaining Loan Amount"
+                        value={this.state.remainingLoanAmount}
+                        type="number"
+                        onChange={this.handleChange}
+                        autoFocus
+                        required={true}
+                      />
+                      <p
+                        className={
+                          this.state.remainingLoanAmountHasError
+                            ? "personal__submision-error"
+                            : "personal__hide"
+                        }
+                      >
+                        {this.state.remainingLoanAmountErrorMessage}
+                      </p>
+                    </div>
+                  </Columns.Column>
+
+                  <Columns.Column size="one-half" />
+                </Columns>
+
+                <button
+                  type="submit"
+                  className="btn btn-lg btn-success btn-block"
+                >
+                  Submit
+                </button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default LoanDetails;
