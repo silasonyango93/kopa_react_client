365ff06f7902f6b0697ba74cbc47d6dca6ee97e5
Silas Onyango <silas.onyango93@gmail.com>
2020-03-08 10:49:33 +0300

    do not render on mobile

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bulma/css/bulma.css";
+import './MainStyle.scss';
 import Login from "./views/authentication/Login.jsx";
 import AdminHome from "./views/admin_home/AdminHome";
 import CompanyOwnerHome from "./views/company_owner_home/CompanyOwnerHome";
 import UserHome from "./views/user_home/UserHome";
+import ErrorPage from "./components/error_page/ErrorPage";
+
 
 class App extends Component {
   render() {
     return (
+        <div>
+          <div className="main-style__error-page">
+            <ErrorPage />
+          </div>
+
+        <div className="main-style__application-body">
       <Router>
         <div>
           <Route path="/" exact component={Login} />
@@ -17,6 +26,8 @@ class App extends Component {
           <Route path="/user_home" exact component={UserHome} />
         </div>
       </Router>
+        </div>
+        </div>
     );
   }
 }
